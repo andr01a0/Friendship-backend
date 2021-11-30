2e670079f72714f31f3f8765dd5f7b5199269316
juaninicolai <juan0719@stud.kea.dk>
2021-11-30 16:33:25 +0100

    create-drop working

--- src/main/java/com/instantcoffee/tech/repo/UserRepo.java
+++ src/main/java/com/instantcoffee/tech/repo/UserRepo.java
@@ -2,7 +2,6 @@ package com.instantcoffee.tech.repo;
 
 import com.instantcoffee.tech.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 public interface UserRepo extends JpaRepository<User,Integer> {
 }
