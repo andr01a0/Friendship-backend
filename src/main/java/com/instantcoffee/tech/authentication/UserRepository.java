5dcc4b07f03ac68f177f9aa3362e2ba8af520be5
André Bittencourt <andrebittencourt@MyMachine.local>
2021-12-01 10:34:55 +0100

    authentication

---
+++ src/main/java/com/instantcoffee/tech/authentication/UserRepository.java
@@ -0,0 +1,12 @@
+package com.instantcoffee.tech.authentication;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  public boolean existsByUsername(String username);
+
+  public User findByUsername(String username);
+}
